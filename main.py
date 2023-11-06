c06af69b5834de132773de5d843e6331e677205b
tdgiangjvl <giangtran@DESKTOP-6K7SDTT.localdomain>
2023-11-06 23:59:38 +0700

    adding cors

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from core.utils import (
     DbHanlder)
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 db_handler = DbHanlder()
 
 @app.get("/tim_van/{words}")
