32848fbe4ad069de85cde41fcf56c5228d6db71c
root <root@cloud20231125254.lanit.com.vn>
2023-12-23 18:09:51 +0000

    adding start UI and CMS server

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,7 @@
 conda activate vuatiengviet_env
 nohup uvicorn main:app --port 8501 --reload --log-config unicorn_log_config.py > fast_api_timvan.log &
+cd /root/vuatiengviet-ui
+pm2 start --name ui "yarn start"
+cd /root/vuatiengviet-cms
+pm2 start --name cms "yarn start"
+sudo service nginx restart
